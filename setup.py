067b6c463e3432cf8e07d58c48a2d36f633c45bd
minii93 <everlastingminii@gmail.com>
2022-08-09 00:20:45 +0900

    Update the version (#21)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysimenv",
-    version="0.0.6",
+    version="0.0.7",
     author="Sangmin Lee",
     author_email="everlastingminii@gmail.com",
     description="A framework for performing numerical simulation of dynamic systems",
