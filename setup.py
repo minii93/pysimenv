abdb72012aea3f3ef884c72a6423a0c8926b3a79
minii93 <everlastingminii@gmail.com>
2022-08-11 09:54:05 +0900

    Update the version to 0.0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysimenv",
-    version="0.0.8",
+    version="0.0.9",
     author="Sangmin Lee",
     author_email="everlastingminii@gmail.com",
     description="A framework for performing numerical simulation of dynamic systems",
