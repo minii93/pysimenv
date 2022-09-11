14f126b006c3ed82bbe00ea358559c528a9e6a54
minii93 <everlastingminii@gmail.com>
2022-09-11 21:25:35 +0900

    Replace deprecated models with new models (#38)

--- pysimenv/missile/model.py
+++ pysimenv/missile/model.py
@@ -1,14 +1,13 @@
-from typing import Union, Optional
+from typing import Union
 
 import numpy as np
 import matplotlib.pyplot as plt
 from pysimenv.core.base import SimObject, DynSystem, ArrayType
-from pysimenv.common.util import wrap_to_pi
 
 
 class PlanarKin(DynSystem):
     def __init__(self, p_0: ArrayType, v_0: ArrayType):
-        super(PlanarKin, self).__init__(initial_states={'p': p_0, 'v': v_0})
+        super(PlanarKin, self).__init__(initial_states={'p': p_0, 'v': v_0}, name="kin")
 
     # implement
     def _deriv(self, p, v, a: np.ndarray):
@@ -52,7 +51,7 @@ class PlanarKin(DynSystem):
 
 class PitchDyn(DynSystem):
     def __init__(self, x_0: ArrayType, L_alp, L_delta, M_alp, M_q, M_delta):
-        super(PitchDyn, self).__init__(initial_states={'x': x_0})
+        super(PitchDyn, self).__init__(initial_states={'x': x_0}, name="pitch_dyn")
         self.L_alp = L_alp
         self.L_delta = L_delta
         self.M_alp = M_alp
@@ -70,7 +69,7 @@ class PitchDyn(DynSystem):
 
 class PitchDynLatAccel(DynSystem):
     def __init__(self, x_0: ArrayType, L_alp, L_del, M_alp, M_q, M_del):
-        super(PitchDynLatAccel, self).__init__(initial_states={'x': x_0})
+        super(PitchDynLatAccel, self).__init__(initial_states={'x': x_0}, name="pitch_dyn")
         self.L_alp = L_alp
         self.L_del = L_del
         self.M_alp = M_alp
@@ -91,6 +90,10 @@ class PitchDynLatAccel(DynSystem):
         return self.state('x')[0]
 
     @property
+    def theta(self) -> float:
+        return self.state('x')[1]
+
+    @property
     def R_vb(self) -> np.ndarray:
         c_alp = np.cos(self.alp)
         s_alp = np.sin(self.alp)
@@ -104,23 +107,31 @@ class PitchDynLatAccel(DynSystem):
 
 
 class PlanarVehicle(SimObject):
-    def __init__(self, kin: PlanarKin):
-        super(PlanarVehicle, self).__init__()
+    def __init__(self, kin: PlanarKin, name="vehicle"):
+        super(PlanarVehicle, self).__init__(name=name)
         self.kin = kin
         self._attach_sim_objs([self.kin])
 
-    # to be implemented
     @property
     def p(self) -> np.ndarray:
-        raise NotImplementedError
+        return self.kin.p
 
+    @property
     def v(self) -> np.ndarray:
-        raise NotImplementedError
+        return self.kin.v
+
+    @property
+    def V(self) -> float:
+        return self.kin.V
+
+    @property
+    def gamma(self) -> float:
+        return self.kin.gamma
 
     def _forward(self, *args, **kwargs) -> Union[None, np.ndarray, dict]:
         raise NotImplementedError
 
-    def plot_path(self, fig_ax=None, label='vehicle', show=False):
+    def plot_path(self, fig_ax=None, label="vehicle", show=False):
         if fig_ax is None:
             fig, ax = plt.subplots()
             ax.set_xlabel("p_x (m)")
@@ -146,24 +157,48 @@ class PlanarVehicle(SimObject):
 
 
 class PlanarMissile(PlanarVehicle):
-    def __init__(self, p_0: ArrayType, V_0: float, gamma_0: float):
+    FLAG_STALLED = 1
+    FLAG_COLLIDED = 2
+
+    def __init__(self, p_0: ArrayType, V_0: float, gamma_0: float, name="missile"):
         v_0 = np.array([V_0*np.cos(gamma_0), V_0*np.sin(gamma_0)])
         kin = PlanarKin(p_0=p_0, v_0=v_0)
-        super(PlanarMissile, self).__init__(kin=kin)
+        super(PlanarMissile, self).__init__(kin=kin, name=name)
+        self.fov_limit = np.inf  # Field-of-view limit
+        self.acc_limit = np.array([-np.inf, np.inf])  # Acceleration limit
+        self.ground_elev = -np.inf  # Ground elevation
+
+    def look_angle(self, lam: float):
+        sigma = self.kin.gamma - lam
+        return sigma
 
     # implement
-    @property
-    def p(self):
-        return self.kin.p
-
-    @property
-    def v(self):
-        return self.kin.v
-
     def _forward(self, a_M_cmd: float):
         a = self.kin.vel_to_inertial(np.array([0., a_M_cmd]))
         self.kin.forward(a=a)
-        self._logger.append(V=self.kin.V, gamma=self.kin.gamma)
+        self._logger.append(t=self.time, V=self.kin.V, gamma=self.kin.gamma)
+
+    # implement
+    def _check_stop_condition(self) -> bool:
+        to_stop = False
+        if self.is_stalled():
+            to_stop = True
+            self.flag = self.FLAG_STALLED
+        if self.is_collided():
+            to_stop = True
+            self.flag = self.FLAG_STALLED
+
+        return to_stop
+
+    def is_stalled(self):
+        return self.kin.V < 1.  # when the speed is less than 1m/s
+
+    def is_collided(self):
+        return self.kin.p[1] < self.ground_elev
+
+    def is_out_of_fov(self, lam: float):
+        sigma = self.look_angle(lam)
+        return abs(sigma) > self.fov_limit
 
     def plot_kin(self, show=False):
         t = self.kin.history('t')
@@ -187,28 +222,45 @@ class PlanarMissile(PlanarVehicle):
             plt.draw()
             plt.pause(0.01)
 
+    def report(self):
+        np.set_printoptions(precision=2, suppress=True)
+
+        print("[{:s}] Position: {:.2f}(m), {:.2f}(m), Speed: {:.2f}(m/s), Flight path angle: {:.2f}(deg)".format(
+            self.name, self.p[0], self.p[1], self.V, np.deg2rad(self.gamma)))
+
+        if self.flag == self.FLAG_OPERATING:
+            print("[{:s}] Status: operating \n".format(self.name))
+        elif self.flag == self.FLAG_STALLED:
+            print("[{:s}] Status: stalled \n".format(self.name))
+        elif self.flag == self.FLAG_COLLIDED:
+            print("[{:s}] Status: collided \n".format(self.name))
+
 
 class PlanarMissileWithPitch(PlanarMissile):
-    def __init__(self, p_0: ArrayType, V_0: float, gamma_0: float, pitch_dyn: PitchDynLatAccel):
-        super(PlanarMissileWithPitch, self).__init__(p_0, V_0, gamma_0)
+    def __init__(self, p_0: ArrayType, V_0: float, gamma_0: float, pitch_dyn: PitchDynLatAccel, name="missile"):
+        super(PlanarMissileWithPitch, self).__init__(p_0, V_0, gamma_0, name=name)
         self.pitch_dyn = pitch_dyn
         self._attach_sim_objs([self.pitch_dyn])
+
+    # override
+    def look_angle(self, lam: float):
+        sigma = self.pitch_dyn.theta - lam
+        return sigma
 
     def _forward(self, a_M_cmd: float):
         a_L = a_M_cmd/np.cos(self.pitch_dyn.alp)
         self.pitch_dyn.forward(a_L=a_L)
 
-        a = self.pitch_dyn.body_to_vel(a_L)
+        a = self.pitch_dyn.body_to_vel(np.array([0., a_L]))
         a = self.kin.vel_to_inertial(a)
         self.kin.forward(a=a)
 
 
 class PlanarMovingTarget(PlanarVehicle):
-    def __init__(self, p_0: ArrayType, V_0: float, gamma_0: float):
+    def __init__(self, p_0: ArrayType, V_0: float, gamma_0: float, name="target"):
         v_0 = np.array([V_0*np.cos(gamma_0), V_0*np.sin(gamma_0)])
         kin = PlanarKin(p_0=p_0, v_0=v_0)
-        super(PlanarMovingTarget, self).__init__(kin=kin)
-        self._attach_sim_objs([self.kin])
+        super(PlanarMovingTarget, self).__init__(kin=kin, name=name)
 
     # implement
     @property
@@ -221,211 +273,4 @@ class PlanarMovingTarget(PlanarVehicle):
 
     def _forward(self):
         self.kin.forward(a=np.zeros(2))
-
-
-class PlanarManVehicle2dof(DynSystem):
-    """
-    A 2-dof model for a maneuvering vehicle in the plane
-    state x: [p_x, p_y, V, gamma]
-    control u: [a_x, a_y]
-    where [p_x, p_y] is the position expressed in the inertial frame and
-    [a_x, a_y] is the acceleration expressed in the velocity frame
-    """
-    def __init__(self, initial_state: ArrayType):
-        super(PlanarManVehicle2dof, self).__init__(initial_states={'x': initial_state})
-        self.name = "planar_man_vehicle"
-
-        def angle_correction_fun(state: np.ndarray) -> np.ndarray:
-            state[3] = wrap_to_pi(state[3])  # gamma should be in [-pi, pi] rad
-            return state
-        self.state_vars['x'].attach_correction_fun(angle_correction_fun)
-
-    def _deriv(self, x: np.ndarray, u: np.ndarray = np.zeros(2)):
-        """
-        :param x: [p_x, p_y, V, gamma]
-        :param u: [a_x, a_y]
-        :return: x_dot
-        """
-        V = x[2]
-        gamma = x[3]
-
-        a_x = u[0]
-        a_y = u[1]
-
-        p_x_dot = V*np.cos(gamma)
-        p_y_dot = V*np.sin(gamma)
-        V_dot = a_x
-
-        if abs(V) < 1e-4:
-            gamma_dot = 0
-        else:
-            gamma_dot = a_y/V
-
-        return {'x': np.array([p_x_dot, p_y_dot, V_dot, gamma_dot])}
-
-    @property
-    def p(self) -> np.ndarray:
-        """
-        :return: [p_x, p_y]
-        """
-        return self.state('x')[0:2]
-
-    @property
-    def v(self) -> np.ndarray:
-        """
-        :return: [v_x, v_y]
-        """
-        x = self.state('x')
-        V = x[2]
-        gamma = x[3]
-
-        return np.array([V*np.cos(gamma), V*np.sin(gamma)])
-
-    @property
-    def V(self) -> float:
-        """
-        :return: V
-        """
-        return self.state('x')[2]
-
-    @property
-    def gamma(self) -> float:
-        """
-        :return: gamma
-        """
-        return self.state('x')[3]
-
-    def plot(self):
-        var_keys = {'x', 'u'}
-        var_ind_dict = {'x': [0, 1, 2, 3], 'u': [0, 1]}
-        var_names_dict = {
-            'x': ['p_x (m)', 'p_y (m)', 'V (m/s)', 'gamma (rad)'],
-            'u': ['a_x (m/s**2)', 'a_y (m/s**2)']
-        }
-        fig_axs = self.default_plot(var_keys=var_keys, var_ind_dict=var_ind_dict, var_names_dict=var_names_dict)
-        return fig_axs
-
-    def plot_path(self, fig_ax=None, show=False):
-        if fig_ax is None:
-            fig, ax = plt.subplots()
-            ax.set_xlabel("p_x (m)")
-            ax.set_ylabel("p_y (m)")
-            ax.grid()
-        else:
-            fig = fig_ax['fig']
-            ax = fig_ax['ax']
-
-        x = self.history('x')
-        p = x[:, 0:2]
-
-        ax.plot(p[:, 0], p[:, 1], label=self.name + " path")
-        ax.set_aspect('equal')
-        ax.legend()
-        fig.tight_layout()
-
-        if show:
-            plt.show()
-        else:
-            plt.draw()
-            plt.pause(0.01)
-        return {'fig': fig, 'ax': ax}
-
-
-class PlanarNonManVehicle2dof(PlanarManVehicle2dof):
-    def __init__(self, initial_state: ArrayType):
-        super(PlanarNonManVehicle2dof, self).__init__(initial_state)
-        self.name = "planar_non_man_vehicle"
-
-    # override
-    def plot(self):
-        var_keys = {'x'}
-        var_ind_dict = {'x': [0, 1, 2, 3]}
-        var_names_dict = {
-            'x': ['p_x (m)', 'p_y (m)', 'V (m/s)', 'gamma (rad)']
-        }
-        fig_axs = self.default_plot(var_keys, var_ind_dict, var_names_dict)
-        return fig_axs
-
-    # override
-    def plot_path(self, fig_ax=None, show=False):
-        if fig_ax is None:
-            fig, ax = plt.subplots()
-            ax.set_xlabel("p_x (m)")
-            ax.set_ylabel("p_y (m)")
-            ax.grid()
-        else:
-            fig = fig_ax['fig']
-            ax = fig_ax['ax']
-
-        x = self.history('x')
-        p = x[:, 0:2]
-
-        if abs(self.V) < 1e-4:
-            ax.plot(p[0, 0], p[0, 1], marker='o', label=self.name)
-        else:
-            ax.plot(p[:, 0], p[:, 1], label=self.name + " path")
-        ax.set_aspect('equal')
-        ax.legend()
-        fig.tight_layout()
-
-        if show:
-            plt.show()
-        else:
-            plt.draw()
-            plt.pause(0.01)
-        return {'fig': fig, 'ax': ax}
-
-
-class PlanarMissile2dof(PlanarManVehicle2dof):
-    NORMAL = 0
-    STALLED = 1
-    COLLIDED = 2
-
-    def __init__(self, initial_state: ArrayType):
-        super(PlanarMissile2dof, self).__init__(initial_state)
-        self.name = "missile"
-        self.fov_limit = np.inf
-        self.acc_limit = np.array([
-            [-np.inf, -np.inf],
-            [np.inf, np.inf]
-        ])
-        self.ground_elevation = -np.inf
-
-    def _forward(self, a_M_cmd: np.ndarray = np.zeros(2)):
-        a_M = np.clip(a_M_cmd, self.acc_limit[0], self.acc_limit[1])
-        super(PlanarMissile2dof, self)._forward(u=a_M)
-        self._logger.append(a_M_cmd=a_M_cmd)
-
-    # implement
-    def _check_stop_condition(self) -> bool:
-        to_stop = False
-        if self.is_stalled():
-            to_stop = True
-            self.flag = self.STALLED
-        if self.is_collided():
-            to_stop = True
-            self.flag = self.COLLIDED
-        return to_stop
-
-    def is_stalled(self) -> bool:
-        return self.V < 10  # when the speed is less than 10m/s
-
-    def is_collided(self) -> bool:
-        return self.p[1] < self.ground_elevation - 0.5
-
-    def is_out_of_view(self, sigma: float) -> bool:
-        return abs(sigma) > self.fov_limit
-
-    def report(self):
-        np.set_printoptions(precision=2, suppress=True)
-        x_M_f = self.state('x')
-
-        print("[{:s}] Final state: {:.2f}(m), {:.2f}(m), {:.2f}(m/s), {:.2f}(deg)".format(
-            self.name, x_M_f[0], x_M_f[1], x_M_f[2], np.rad2deg(x_M_f[3])))
-
-        if self.flag == self.NORMAL:
-            print("[{:s}] Status: normal \n".format(self.name))
-        elif self.flag == self.STALLED:
-            print("[{:s}] Status: stalled \n".format(self.name))
-        elif self.flag == self.COLLIDED:
-            print("[{:s}] Status: collided \n".format(self.name))
+        self._logger.append(t=self.time, V=self.kin.V, gamma=self.kin.gamma)
